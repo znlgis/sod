eff2cafe8e08bcd7ca9d4cb039e5fe422baf5d0d
深蓝医生 <2637208+bluedoctor@users.noreply.github.com>
2015-03-08 07:39:10 +0000

    PWMIS.Web 项目之 SOD框架数据控件支持设计时属性选择器

---
+++ Lib/PWMIS.Web/Web/PropertyUITypeEditor.cs
@@ -0,0 +1,48 @@
+﻿using PWMIS.Common;
+using PWMIS.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace PWMIS.Web
+{
+    public class PropertyUITypeEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(System.ComponentModel.ITypeDescriptorContext context)
+        {
+            if (context != null && context.Instance != null)
+            {
+                return UITypeEditorEditStyle.Modal;
+            }
+
+            return base.GetEditStyle(context);
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            IWindowsFormsEditorService editorService = null;
+
+            if (context != null && context.Instance != null && provider != null)
+            {
+                editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+                if (editorService != null)
+                {
+                    IDataControl control = (IDataControl)context.Instance;
+                    PropertySelectorForm dlg = new PropertySelectorForm(control);
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        value = dlg.PropertyName;
+                        return value;
+                    }
+                }
+            }
+
+            return value;
+        }
+
+    }
+}
