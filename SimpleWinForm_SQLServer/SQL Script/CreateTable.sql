beade4a33d3b70ae8b66654cb085ed0aa7a0e5fc
深蓝医生 <2637208+bluedoctor@users.noreply.github.com>
2015-03-10 14:00:37 +0000

    添加简单SQL Server Demo

---
+++ SimpleWinForm_SQLServer/SQL Script/CreateTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserID]       INT             NOT NULL,
+    [UserName]     NVARCHAR (50)   NULL,
+    [UserType]     INT             NULL,
+    [RegisterDate] DATETIME        NULL,
+    [Expenditure]  DECIMAL (18, 4) NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+INSERT INTO [dbo].[User] ([UserID], [UserName], [UserType], [RegisterDate], [Expenditure]) VALUES (1, N'Admin', NULL, N'1990-01-01 00:00:00', NULL)
