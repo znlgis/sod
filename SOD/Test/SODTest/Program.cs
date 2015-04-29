c61fb20321414a1c7e698f0ee21b52ae9e41f793
dth_cp <SND\dth_cp>
2015-04-29 07:05:55 +0000

    修改了条件比较过程中，OQL字段堆栈 的处理方式，解决了“调试陷阱”的问题

--- SOD/Test/SODTest/Program.cs
+++ SOD/Test/SODTest/Program.cs
@@ -14,33 +14,35 @@ namespace SODTest
         static void Main(string[] args)
         {
             SalesOrder model = new SalesOrder();
-            //model.iOrderTypeID = "123";
+            model.iOrderTypeID = "123";
 
             //string orderTypeID = model.iOrderTypeID;
             BCustomer bCustomer = new BCustomer();
 
-            OQLCompareFunc<SalesOrder,BCustomer> cmpFun = (cmp,S,C) =>
+            OQLCompareFunc<BCustomer,SalesOrder> cmpFun = (cmp,C,S) =>
             {
                 OQLCompare cmpResult = null;
-                cmpResult = cmp.Comparer<int>(S.iBillID, OQLCompare.CompareType.Equal, 0);
+                cmpResult = cmp.Comparer(S.iBillID, OQLCompare.CompareType.Equal, 1);
+                
                 if (!string.IsNullOrEmpty(S.iOrderTypeID))
-                    cmpResult = cmpResult & cmp.Comparer<string>(S.iOrderTypeID, OQLCompare.CompareType.Equal, S.iOrderTypeID);
+                    cmpResult = cmpResult & cmp.Comparer(S.iOrderTypeID, OQLCompare.CompareType.Equal, S.iOrderTypeID);
 
                 int iCityID = 39;
                 //由于调用了关联实体类的 S.iOrderTypeID 用于条件比较，所以下面需要调用 cmp.NewCompare()
                 //cmpResult = cmpResult & cmp.NewCompare().Comparer<int>(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
                 //感谢网友 红枫星空 发现此问题
 
-                //或者继续采用下面的写法，但是必须确定Comparer 方法第一个参数调用为实体类属性，而不是待比较的值
-                cmpResult = cmpResult & cmp.Comparer<int>(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
+                //或者继续采用下面的写法，但是必须确保 Comparer 方法第一个参数调用为实体类属性，而不是待比较的值
+                cmpResult = cmpResult & cmp.Comparer(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
                 return cmpResult;
             };
            
 
-            OQL oQL = OQL.From(model).LeftJoin(bCustomer).On(model.iCustomerID, bCustomer.ISID)
-                .Select()
-                .Where(cmpFun)
-                .OrderBy(model.iBillID, "desc")
+            OQL oQL = OQL.From(model)
+                    .LeftJoin(bCustomer).On(model.iCustomerID, bCustomer.ISID)
+                    .Select()
+                    .Where(cmpFun)
+                    .OrderBy(model.iBillID, "desc")
                 .END;
 
             Console.WriteLine(oQL);
