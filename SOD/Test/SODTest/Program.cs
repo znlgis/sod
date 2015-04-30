83a26ae4c13d85136bb9e3763b894e42d222c353
dth_cp <SND\dth_cp>
2015-04-30 05:08:10 +0000

    更新动态构造查询条件的示例

--- SOD/Test/SODTest/Program.cs
+++ SOD/Test/SODTest/Program.cs
@@ -7,7 +7,8 @@ using PWMIS.DataMap.Entity;
 namespace SODTest
 {
     /// <summary>
-    /// OQL 多实体类查询 动态条件构造测试 网友  红枫星空  提供
+    /// OQL 多实体类查询 动态条件构造测试 ，原始程序由网友  红枫星空  提供
+    /// <seealso cref="http://www.cnblogs.com/bluedoctor/p/3225176.html"/>
     /// </summary>
     class Program
     {
@@ -15,29 +16,10 @@ namespace SODTest
         {
             SalesOrder model = new SalesOrder();
             //model.iOrderTypeID = "123";
-
-            //string orderTypeID = model.iOrderTypeID;
             BCustomer bCustomer = new BCustomer();
 
-            OQLCompareFunc<BCustomer,SalesOrder> cmpFun = (cmp,C,S) =>
-            {
-                OQLCompare cmpResult = null;
-                cmpResult = cmp.Comparer(S.iBillID, OQLCompare.CompareType.Equal, 1);
-                
-                if (!string.IsNullOrEmpty(S.iOrderTypeID))
-                    cmpResult = cmpResult & cmp.Comparer(S.iOrderTypeID, OQLCompare.CompareType.Equal, S.iOrderTypeID);
-
-                int iCityID = 30;
-                //由于调用了关联实体类的 S.iOrderTypeID 用于条件比较，所以下面需要调用 cmp.NewCompare()
-                //cmpResult = cmpResult & cmp.NewCompare().Comparer<int>(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
-                //感谢网友 红枫星空 发现此问题
-
-                //或者继续采用下面的写法，但是必须确保 Comparer 方法第一个参数调用为实体类属性，而不是待比较的值
-                //且第一个参数的值不能等于第三个参数的值，否则需要调用NewCompare() 方法
-                cmpResult = cmpResult & cmp.Comparer(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
-                return cmpResult;
-            };
-           
+            //请注意方法 GetCondtion1，GetCondtion2 中变量 iCityID 的不同而带来的构造条件语句的不同
+            OQLCompareFunc<BCustomer, SalesOrder> cmpFun = GetCondtion1();
 
             OQL oQL = OQL.From(model)
                     .LeftJoin(bCustomer).On(model.iCustomerID, bCustomer.ISID)
@@ -48,7 +30,49 @@ namespace SODTest
 
             Console.WriteLine(oQL);
             Console.WriteLine(oQL.PrintParameterInfo());
+            //此OQL 可以由 EntityContainer 对象的方法执行
             Console.ReadLine();
+        }
+
+        private static OQLCompareFunc<BCustomer, SalesOrder> GetCondtion1()
+        {
+            OQLCompareFunc<BCustomer, SalesOrder> cmpFun = (cmp, C, S) =>
+            {
+                OQLCompare cmpResult = null;
+                cmpResult = cmp.Comparer(S.iBillID, OQLCompare.CompareType.Equal, 1);
+
+                if (!string.IsNullOrEmpty(S.iOrderTypeID))
+                    cmpResult = cmpResult & cmp.Comparer(S.iOrderTypeID, OQLCompare.CompareType.Equal, S.iOrderTypeID);
+
+                int iCityID = 30;
+                //由于调用了关联实体类的 S.iOrderTypeID 用于条件比较，所以下面的比较需要注意：
+                //必须确保 Comparer 方法第一个参数调用为实体类属性，而不是待比较的值
+                //    且第一个参数的值不能等于第三个参数的值，否则需要调用NewCompare() 方法
+                cmpResult = cmpResult & cmp.Comparer(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
+                return cmpResult;
+            };
+            return cmpFun;
+        }
+
+        private static OQLCompareFunc<BCustomer, SalesOrder> GetCondtion2()
+        {
+            OQLCompareFunc<BCustomer, SalesOrder> cmpFun = (cmp, C, S) =>
+            {
+                OQLCompare cmpResult = null;
+                cmpResult = cmp.Comparer(S.iBillID, OQLCompare.CompareType.Equal, 1);
+
+                if (!string.IsNullOrEmpty(S.iOrderTypeID))
+                    cmpResult = cmpResult & cmp.Comparer(S.iOrderTypeID, OQLCompare.CompareType.Equal, S.iOrderTypeID);
+
+                int iCityID = 0;
+                //由于调用了关联实体类的 S.iOrderTypeID 用于条件比较【上面的IsNullOrEmpty 调用】，并且C.iCityID==iCityID==0 ，
+                //所以下面需要调用 cmp.NewCompare()，以清除OQL字段堆栈中的数据对Comparer 方法的影响 
+                //感谢网友 红枫星空 发现此问题
+                //如果 C.iCityID != iCityID ,尽管上面调用了关联实体类的属性，但 Comparer 方法不受影响，也不需要调用 NewCompare 方法
+                cmpResult = cmpResult & cmp.NewCompare().Comparer(C.iCityID, OQLCompare.CompareType.Equal, iCityID);
+                return cmpResult;
+            };
+            return cmpFun;
         }
     }
 }
