206a1df99827bcea4c72748e044fc372f18850f5
dth_cp <SND\dth_cp>
2016-01-28 03:11:31 +0000

    框架AdoHelper增加通用日志接口

---
+++ SOD/Lib/PWMIS.Core/Interface/ICommonLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PWMIS.Common
+{
+    /// <summary>
+    /// 通用日志接口
+    /// </summary>
+    public interface ICommandLog
+    {
+        void WriteLog(string msg, string who);
+    }
+}
