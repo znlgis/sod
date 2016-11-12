6b981859043f354a23747b4f0b1032d694dc1450
深蓝医生 <2637208+bluedoctor@users.noreply.github.com>
2016-11-12 05:44:22 +0000

    SOD MVVM Example

---
+++ Example/WinFormMvvm/WinFormMvvm.Model/UserModel.cs
@@ -0,0 +1,52 @@
+﻿using PWMIS.DataMap.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormMvvm.Model
+{
+    public class UserModel
+    {
+        private static int index = 0;
+        private LocalDbContext context;
+        public UserModel()
+        {
+            context = new LocalDbContext();
+        }
+
+        public List<UserEntity> GetAllUsers()
+        {
+            return OQL.From<UserEntity>().ToList(context.CurrentDataBase);
+        }
+        public void UpdateUser(UserEntity user)
+        {
+            int count= context.Update<UserEntity>(user);
+        }
+
+        public void AddUsers(IList<UserEntity> users)
+        {
+            int count = context.AddList(users);
+        }
+
+        public void SubmitUser(UserEntity user)
+        {
+           int count = context.Add(user);
+        }
+
+        public void RemoveUser(UserEntity user)
+        {
+            int count = context.Remove(user);
+        }
+
+        public UserEntity CreateNewUser(string userName="NoName")
+        {
+            return new UserEntity()
+            {
+                 ID= ++index,
+                 Name =userName
+            };
+        }
+    }
+}
