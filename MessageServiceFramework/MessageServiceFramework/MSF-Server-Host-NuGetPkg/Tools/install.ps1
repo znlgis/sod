5a32407bded4d07a8b86ef69e71f29bc6a2b0803
dth_cp <SND\dth_cp>
2017-09-18 09:37:33 +0000

    修改Nuget打包程序

--- MessageServiceFramework/MessageServiceFramework/MSF-Server-Host-NuGetPkg/Tools/install.ps1
+++ MessageServiceFramework/MessageServiceFramework/MSF-Server-Host-NuGetPkg/Tools/install.ps1
@@ -1,2 +1,6 @@
 ﻿param($installPath, $toolsPath, $package, $project)
-Write-Host "Show Test:" $installPath
+$hostDir= Join-Path $([System.IO.Path]::GetDirectoryName($dte.Solution.FileName)) Host
+if(!(Test-Path $hostDir) ){ 
+md  $hostDir
+}
+# 复制文件到解决方案的 Host目录
