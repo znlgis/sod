6645bdf268967ef0c836e1ccd6ce21813c2b0a55
dth_cp <SND\dth_cp>
2017-09-22 09:42:46 +0000

    编辑 MSF nuget

--- MessageServiceFramework/MessageServiceFramework/MSF-Server-Host-NuGetPkg/Tools/install.ps1
+++ MessageServiceFramework/MessageServiceFramework/MSF-Server-Host-NuGetPkg/Tools/install.ps1
@@ -16,4 +16,13 @@ if(! (Test-Path $host_exe_target)) { Copy-Item $host_exe_src $hostDir }
 if(! (Test-Path $host_pdb_target)) { Copy-Item $host_pdb_src $hostDir }
 if(! (Test-Path $updateSrv_target)) { Copy-Item $updateSrv_src $hostDir }
 
-# 修改编译后事件，复制文件，启动主进程
+# 修改编译前后后事件，复制文件，启动主进程
+$preBEvent=$project.Properties.Item("PreBuildEvent")
+if(! $preBEvent.Value.Contains("PreCompile.bat")) { 
+ $preBEvent.Value= $preBEvent.Value+"`n start `"`" `"`$(SolutionDir)Host\PreCompile.bat`""
+}
+
+$postBEvent= $project.Properties.Item("PostBuildEvent")
+if(! $postBEvent.Value.Contains("(TargetDir)*.*")) { 
+ $postBEvent.Value= $postBEvent.Value+"`n copy /y `"`$(TargetDir)*.*`" `"`$(SolutionDir)Host `n cd "$(SolutionDir)ServiceHost\BizServer\ServiceHost" start "MessageService Host"  "SucessCompiled.vbs""
+}
