89f77ed783169beebde4d8839ded8426ce622e42
dth <dengtaihua@dxn.com.cn>
2018-03-12 18:12:34 +0800

    增加事务日志复制的功能和示例

---
+++ src/SOD/DataReplicationExample/Program.cs
@@ -0,0 +1,94 @@
+﻿using PWMIS.Core.Extensions;
+using PWMIS.DataProvider.Adapter;
+using PWMIS.DataProvider.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DataReplicationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====**************** PDF.NET SOD 控制台测试程序 **************====");
+            Assembly coreAss = Assembly.GetAssembly(typeof(AdoHelper));//获得引用程序集
+            Console.WriteLine("框架核心程序集 PWMIS.Core Version:{0}", coreAss.GetName().Version.ToString());
+            Console.WriteLine();
+
+            AdoHelper sourceDb = AdoHelper.CreateHelper("SourceDB");
+            AdoHelper targetDb = AdoHelper.CreateHelper("ReplicatedDB");
+
+            Console.WriteLine(" 源数据库配置信息：\r\n  当前使用的数据库类型是：{0}\r\n  连接字符串为:{1}"
+                , sourceDb.CurrentDBMSType, sourceDb.ConnectionString);
+            Console.WriteLine("目标数据库配置信息：\r\n  当前使用的数据库类型是：{0}\r\n  连接字符串为:{1}"
+               , targetDb.CurrentDBMSType, targetDb.ConnectionString);
+
+            Console.WriteLine("\r\n  请确保数据库服务器和数据库是否有效(如果是SqlServer，框架会自动创建数据库)，\r\n继续请回车，退出请输入字母 Q .");
+            Console.WriteLine("=====Power by Bluedoctor,2018.3.12 http://www.pwmis.com/sqlmap ====");
+            string read = Console.ReadLine();
+            if (read.ToUpper() == "Q")
+                return;
+
+            
+            //初始化数据库
+            TransLogDbContext sourceCtx = new TransLogDbContext(sourceDb);
+            sourceCtx.CheckTableExists<UserEntity>();
+            Console.WriteLine("源数据库初始化检查成功----");
+            //在源数据库注册事务日志命令处理器
+            sourceDb.RegisterCommandHandle(new TransactionLogHandle());
+
+            //在源添加和修改数据
+            UserEntity user = new UserEntity();
+            user.Name = "zhang san";
+            user.Height = 1.8f;
+            user.Birthday = new DateTime(1980, 1, 1);
+            user.Sex = true;
+            sourceCtx.Add(user);
+
+            user.Name = "lisi";
+            user.Height = 1.6f;
+            user.Birthday = new DateTime(1982, 3, 1);
+            user.Sex = false;
+            sourceCtx.Add(user);
+
+            user.Birthday= new DateTime(1990, 1, 1);
+            sourceCtx.Update(user);
+
+            Console.WriteLine("测试 生成事务日志 成功！（此操作将写入事务日志信息到数据库中。）");
+            Console.WriteLine("准备复制数据");
+
+            //初始化目标数据库
+            TransLogDbContext targetCtx = new TransLogDbContext(targetDb);
+            targetCtx.CheckTableExists<UserEntity>();
+            Console.WriteLine("目标数据库初始化检查成功----");
+
+            //从源读取数据
+            int count= sourceCtx.ReadLog(10, list => {
+                //复制数据到目标库
+                foreach (var log in list)
+                {
+                    bool result = targetCtx.DataReplication(log);
+                    if (!result)
+                    {
+                        Console.WriteLine("****复制数据遇到错误，日志ID:{0}，命令：{1}，错误原因：{2}", log.CommandID, log.CommandText, targetCtx.ErrorMessage);
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("操作成功，日志ID:{0}，执行状态：{1}， 命令：{2}",log.CommandID,targetCtx.CurrentStatus,log.CommandText);
+                    }
+                }
+                return true;
+
+            });
+            Console.WriteLine();
+            Console.WriteLine("操作完成，从源数据库读取 {0}条记录。", count);
+            Console.WriteLine();
+            Console.WriteLine("测试全部完成，按回车键退出。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/SOD/Lib/PWMIS.Core.Extensions/TransLogDbContext.cs
@@ -0,0 +1,139 @@
+﻿using PWMIS.DataMap.Entity;
+using PWMIS.DataProvider.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PWMIS.Core.Extensions
+{
+    /// <summary>
+    /// 复制状态
+    /// </summary>
+    public enum ReplicationStatus
+    {
+        /// <summary>
+        /// 未知状态
+        /// </summary>
+        UnKnown,
+        /// <summary>
+        /// 日志已经存在
+        /// </summary>
+        LogExists,
+        /// <summary>
+        /// 复制成功
+        /// </summary>
+        Succeed,
+        /// <summary>
+        /// 复制过程发生错误，请查看错误信息
+        /// </summary>
+        Error
+    }
+
+    /// <summary>
+    /// 管理事务日志的数据访问上下文对象
+    /// </summary>
+    public class TransLogDbContext : DbContext
+    {
+        /// <summary>
+        /// 操作过程中的错误信息
+        /// </summary>
+        public string ErrorMessage { get; private set; }
+        /// <summary>
+        /// 当前操作的复制状态
+        /// </summary>
+        public ReplicationStatus CurrentStatus { get; private set; }
+
+        /// <summary>
+        /// 以一个数据上下文对象初始化本类
+        /// </summary>
+        /// <param name="db"></param>
+        public TransLogDbContext(AdoHelper db) : base(db)
+        { }
+
+        /// <summary>
+        /// 以连接字符串名字来初始化本类
+        /// </summary>
+        /// <param name="connName">应用程序配置文件中的连接字符串名字</param>
+        public TransLogDbContext(string connName) : base(connName)
+        { }
+
+        protected override bool CheckAllTableExists()
+        {
+            base.CheckTableExists<MyCommandLogEntity>();
+            return true;
+        }
+
+        /// <summary>
+        /// 按照顺序读取事务日志并进行处理
+        /// </summary>
+        /// <param name="pageSize">每次要读取的日志页大小</param>
+        /// <param name="func">处理日志的自定义方法，如果返回假则不再继续读取处理</param>
+        /// <returns>返回已经读取的记录数量</returns>
+        public int ReadLog(int pageSize,Func<List<MyCommandLogEntity>,bool> func )
+        {
+            int pageNumber = 1;
+            int readCount = 0;
+            while (true)
+            {
+                var list = OQL.From<MyCommandLogEntity>()
+                .Select()
+                .OrderBy((o, p) => o.Asc(p.CommandID))
+                .Limit(pageSize, pageNumber)
+                .ToList(this.CurrentDataBase);
+                if (list.Count == 0)
+                    break;
+                readCount += list.Count;
+
+                if (!func(list))
+                    break;
+                if (list.Count < pageSize)
+                    break;
+                pageNumber++;
+            }
+            return readCount;
+        }
+
+        /// <summary>
+        /// 将制定的数据复制到当前数据访问对象关联的数据库中
+        /// </summary>
+        /// <param name="log">事务日志实体</param>
+        /// <returns>如果数据已经存在或者复制成功，返回真；如果遇到错误，返回假</returns>
+        public bool DataReplication(MyCommandLogEntity log)
+        {
+            this.ErrorMessage = string.Empty;
+            this.CurrentStatus = ReplicationStatus.UnKnown;
+
+            var query = this.NewQuery<MyCommandLogEntity>();
+            if (query.ExistsEntity(log))
+            {
+                this.CurrentStatus = ReplicationStatus.LogExists;
+                return true;
+            }
+            else
+            {
+                string errorMessage;
+                MyCommandLogEntity newLog = new MyCommandLogEntity();
+                newLog.CommandID = log.CommandID;
+                newLog.CommandName = log.CommandName;
+                newLog.CommandText = log.CommandText;
+                newLog.CommandType = log.CommandType;
+                newLog.ExecuteTime = DateTime.Now;
+                newLog.LogFlag = 2;//表示已经复制的状态
+
+                bool result= Transaction(ctx => {
+                    query.Insert(newLog);
+                    
+                    //解析得到真正的命令参数信息
+                    var paras= log.ParseParameter(this.CurrentDataBase);
+                    int count= this.CurrentDataBase.ExecuteNonQuery(log.CommandText, log.CommandType, paras);
+                    //可以在此处考虑引发处理后事件
+                }, out errorMessage);
+
+                this.ErrorMessage = errorMessage;
+                this.CurrentStatus = result ? ReplicationStatus.Succeed : ReplicationStatus.Error;
+                return result;
+            }
+        }
+    }
+}
