b88f833950d738c06a3bdf5e25572fb50a34679a
znlgis <znlgis@foxmail.com>
2019-06-12 22:22:00 +0800

    升级NUGET

--- src/SOD/NuGet/pushnupkg.bat
+++ src/SOD/NuGet/pushnupkg.bat
@@ -1,13 +1,13 @@
-nuget push PDF.NET.SOD.Core.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.Access.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.MemoryStorage.Extensions.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.MySQL.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.ODataClient.Extensions.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.Oracle.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.PostgreSQL.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.SQLite.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.SqlServerCe.Provider.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.Web.Extensions.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
-nuget push PDF.NET.SOD.WinForm.Extensions.5.6.2.1231.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.Core.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.Access.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.MemoryStorage.Extensions.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.MySQL.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.ODataClient.Extensions.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.Oracle.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.PostgreSQL.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.SQLite.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.SqlServerCe.Provider.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.Web.Extensions.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
+nuget push PDF.NET.SOD.WinForm.Extensions.5.6.3.612.nupkg -Source https://www.nuget.org/api/v2/package 
 pause
